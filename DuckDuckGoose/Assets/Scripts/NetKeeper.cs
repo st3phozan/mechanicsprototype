2203d8a4140dd2f2c204e2d9dc24174ca4915b1c
Stephen Ozan <stephenozan@utexas.edu>
2024-10-01 09:32:43 -0500

    final

---
+++ DuckDuckGoose/Assets/Scripts/NetKeeper.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NetKeeper : MonoBehaviour
+{
+    public float score = 0;
+
+    public List<GameObject> ball = new List<GameObject>();
+
+    public Controller ctrl;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "ball")
+        {
+            //gameRestarted = true;
+            ctrl.score += 1;
+
+
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "ball")
+        {
+            //gameRestarted = true;
+            ctrl.score -= 1;
+
+
+        }
+
+    }
+}
